5ef3dc72c77631574bfc8b3a4034ffa438a6e20d
Subhadeep Ghosh <subhadeepghosh1270@gmail.com>
2026-02-26 14:44:21 +0530

    made change in axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
-
 export const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
   withCredentials: true,
 });
